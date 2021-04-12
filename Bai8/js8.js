ec552d337c44488e62a468acae2e510c93c81deb
TamNguyenCG <ngductam91@gmail.com>
2021-04-12 11:05:01 +0700

    If-else2-Bai8

---
+++ Bai8/js8.js
@@ -0,0 +1,6 @@
+let number=+prompt('Nhập một số: ')
+if (number>0 && number<120){
+    alert(number+' có thể là tuổi của một người')
+}else{
+    alert(number+' không phải là số tuổi của một người')
+}
